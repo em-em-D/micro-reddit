ec430ef851ea01c6cf5f4f6317500d1fa483232a
em-em-D <michaeldamulak@gmail.com>
2019-07-16 13:28:47 +0200

    new changes added

--- db/migrate/20190716070931_adding_post_id_and_user_id_to_comment.rb
+++ db/migrate/20190716070931_adding_post_id_and_user_id_to_comment.rb
@@ -1,5 +1,7 @@
 class AddingPostIdAndUserIdToComment < ActiveRecord::Migration[5.2]
   def change
+    remove_column :comments, :user_id, :integer
+    remove_column :comments, :post_id, :integer
     add_reference :comments, :user_id, foreign_key: true
     add_reference :comments, :post_id, foreign_key: true
   end
